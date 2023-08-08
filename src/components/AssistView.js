ee9ae61a9a1d94ebe6baf1c48313f5f0d32bbfee
zzicarus <2450606426@qq.com>
2023-08-08 21:19:20 +0800

    update the AssistView

--- src/components/AssistView.js
+++ src/components/AssistView.js
@@ -1,205 +1,142 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { withStyles } from '@material-ui/core/styles';
-import TableCell from '@material-ui/core/TableCell';
-import Paper from '@material-ui/core/Paper';
-import { AutoSizer, Column, Table } from 'react-virtualized';
+import * as React from 'react';
+import { DataGrid, GridToolbarFilterButton,GridToolbarColumnsButton, GridToolbarContainer, GridToolbarExport } from '@material-ui/data-grid';
 
-const styles = (theme) => ({
-  flexContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    boxSizing: 'border-box',
+// 数据
+// 这种形式的 table id属性必须存在不要动
+// id 不能够用相同的，否则后面的id会替代前面的
+// name	rating genre	year	released	score	votes	director	writer	star	country	budget	gross	company	runtime
+const columns = [
+  { field: 'id', 
+    headerName: 'ID', 
+    width: 90 ,
+    /*悬浮在上面显示的东西*/
+    description: ''
   },
-  table: {
-    // temporary right-to-left patch, waiting for
-    // https://github.com/bvaughn/react-virtualized/issues/454
-    '& .ReactVirtualized__Table__headerRow': {
-      flip: false,
-      paddingRight: theme.direction === 'rtl' ? '0 !important' : undefined,
-    },
+  {
+    field: 'name',
+    headerName: '电影名称',
+    width: 150,
   },
-  tableRow: {
-    cursor: 'pointer',
+  {
+    field: 'rating',
+    headerName: '分级',
+    width: 90,
   },
-  tableRowHover: {
-    '&:hover': {
-      backgroundColor: theme.palette.grey[200],
-    },
+  {
+    field: 'genre',
+    headerName: '种类',
+    width: 90,
   },
-  tableCell: {
-    flex: 1,
+  {
+    field: 'year',
+    headerName: '发行年份',
+    type:'number',
+    description:'发行年份',
+    width: 110,
   },
-  noClick: {
-    cursor: 'initial',
+  {
+    field: 'released',
+    headerName: '发行日期',
+    sortable:false,
+    width: 110,
   },
-});
-
-class MuiVirtualizedTable extends React.PureComponent {
-  static defaultProps = {
-    headerHeight: 48,
-    rowHeight: 48,
-  };
-
-  getRowClassName = ({ index }) => {
-    const { classes, onRowClick } = this.props;
-
-    return clsx(classes.tableRow, classes.flexContainer, {
-      [classes.tableRowHover]: index !== -1 && onRowClick != null,
-    });
-  };
-
-  cellRenderer = ({ cellData, columnIndex }) => {
-    const { columns, classes, rowHeight, onRowClick } = this.props;
-    return (
-      <TableCell
-        component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, {
-          [classes.noClick]: onRowClick == null,
-        })}
-        variant="body"
-        style={{ height: rowHeight }}
-        align={(columnIndex != null && columns[columnIndex].numeric) || false ? 'right' : 'left'}
-      >
-        {cellData}
-      </TableCell>
-    );
-  };
-
-  headerRenderer = ({ label, columnIndex }) => {
-    const { headerHeight, columns, classes } = this.props;
-
-    return (
-      <TableCell
-        component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
-        variant="head"
-        style={{ height: headerHeight }}
-        align={columns[columnIndex].numeric || false ? 'right' : 'left'}
-      >
-        <span>{label}</span>
-      </TableCell>
-    );
-  };
-
-  render() {
-    const { classes, columns, rowHeight, headerHeight, ...tableProps } = this.props;
-    return (
-      <AutoSizer>
-        {({ height, width }) => (
-          <Table
-            height={height}
-            width={width}
-            rowHeight={rowHeight}
-            gridStyle={{
-              direction: 'inherit',
-            }}
-            headerHeight={headerHeight}
-            className={classes.table}
-            {...tableProps}
-            rowClassName={this.getRowClassName}
-          >
-            {columns.map(({ dataKey, ...other }, index) => {
-              return (
-                <Column
-                  key={dataKey}
-                  headerRenderer={(headerProps) =>
-                    this.headerRenderer({
-                      ...headerProps,
-                      columnIndex: index,
-                    })
-                  }
-                  className={classes.flexContainer}
-                  cellRenderer={this.cellRenderer}
-                  dataKey={dataKey}
-                  {...other}
-                />
-              );
-            })}
-          </Table>
-        )}
-      </AutoSizer>
-    );
-  }
-}
-
-MuiVirtualizedTable.propTypes = {
-  classes: PropTypes.object.isRequired,
-  columns: PropTypes.arrayOf(
-    PropTypes.shape({
-      dataKey: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      numeric: PropTypes.bool,
-      width: PropTypes.number.isRequired,
-    }),
-  ).isRequired,
-  headerHeight: PropTypes.number,
-  onRowClick: PropTypes.func,
-  rowHeight: PropTypes.number,
-};
-
-const VirtualizedTable = withStyles(styles)(MuiVirtualizedTable);
-
-// ---
-
-const sample = [
-  ['Frozen yoghurt', 159, 6.0, 24, 4.0],
-  ['Ice cream sandwich', 237, 9.0, 37, 4.3],
-  ['Eclair', 262, 16.0, 24, 6.0],
-  ['Cupcake', 305, 3.7, 67, 4.3],
-  ['Gingerbread', 356, 16.0, 49, 3.9],
+  {
+    field: 'score',
+    headerName: '得分',
+    width: 90,
+  },
+  {
+    field: 'votes',
+    headerName: 'VOTES',
+    width: 90,
+  },
+  {
+    field: 'director',
+    headerName: '导演',
+    width: 90,
+  },
+  {
+    field: 'writer',
+    headerName: '作者',
+    width: 110,
+    description:'一般指剧本作者或者原作者'
+  },
+  {
+    field: 'star',
+    headerName: '主演',
+    width: 110,
+  },
+  {
+    field: 'country',
+    headerName: '国家',
+    width: 110,
+  },
+  {
+    field: 'budget',
+    headerName: '预算',
+    width: 110,
+  },
+  {
+    field: 'gross',
+    headerName: '利润',
+    width: 110,
+  },
+  {
+    field: 'company',
+    headerName: '发行公司',
+    width: 110,
+    description:'发行公司',
+  },
+  {
+    field: 'runtime',
+    headerName: '影片时长',
+    width: 110,
+    description:'影片时长',
+  },
 ];
 
-function createData(id, dessert, calories, fat, carbs, protein) {
-  return { id, dessert, calories, fat, carbs, protein };
-}
-
-const rows = [];
-
-for (let i = 0; i < 200; i += 1) {
-  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-  rows.push(createData(i, ...randomSelection));
-}
-
-export default function ReactVirtualizedTable() {
+// 实现excel数据导入
+// name	rating genre	year	released	score	votes	director	writer	star	country	budget	gross	company	runtime
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+// 自定义的菜单
+function CustomToolbar() {
   return (
-    <Paper style={{ height: 400, width: '100%' }}>
-      <VirtualizedTable
-        rowCount={rows.length}
-        rowGetter={({ index }) => rows[index]}
-        columns={[
-          {
-            width: 200,
-            label: 'Dessert',
-            dataKey: 'dessert',
-          },
-          {
-            width: 120,
-            label: 'Calories\u00A0(g)',
-            dataKey: 'calories',
-            numeric: true,
-          },
-          {
-            width: 120,
-            label: 'Fat\u00A0(g)',
-            dataKey: 'fat',
-            numeric: true,
-          },
-          {
-            width: 120,
-            label: 'Carbs\u00A0(g)',
-            dataKey: 'carbs',
-            numeric: true,
-          },
-          {
-            width: 120,
-            label: 'Protein\u00A0(g)',
-            dataKey: 'protein',
-            numeric: true,
-          },
-        ]}
+    <GridToolbarContainer>
+      <GridToolbarColumnsButton />
+      <GridToolbarFilterButton/>
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+}
+
+export default function DataTable() {
+  return (
+    <div style={{ height: '100%', width: '100%' }}>
+      <DataGrid
+        //禁用sort
+        
+        rows={rows}
+        columns={columns}
+        pageSize={100}
+        // 菜单栏
+        components={{
+          Toolbar: CustomToolbar,
+        }}
+        // checkboxSelection  实现选择
+        // disableSelectionOnClick
+        
+        
       />
-    </Paper>
+    </div>
   );
 }
